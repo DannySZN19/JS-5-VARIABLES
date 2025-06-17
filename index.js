702ea470128b5db79396c5d14c26631860de7462
DannySZN19 <danielchidi100@gmail.com>
2025-06-18 00:01:44 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+/* <strings> */
+const firstName = "Daniel"
+
+const lastName = "Chidi"
+
+const sisterName = "Pamela"
+
+const dadName = "Frank"
+
+const nieceName ="Zita"
+
+console.log(firstName.length)
+
+console.log(lastName.toLowerCase())
+
+console.log(sisterName.includes("P"))
+
+console.log(dadName.endsWith("k"))
+
+console.log(nieceName.charAt("0"))
+
+/* <numbers> */
+const CGPA = 3
+
+const price = 20
+
+const salary = 70000
+
+const score = 45
+
+const temperature = 78
+
+console.log(CGPA.length)
+
+/* <booleans> */
+const isActive = false
+
+const isAdmin = true
+
+const isPremiumUser = false
+
+const hasDiscount = true
+
+const isMember = false
+
+
+
+
+
+
+
+
+
+
